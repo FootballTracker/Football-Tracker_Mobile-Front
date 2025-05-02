ffe030b64a7e34ed6520d0802a7a02d69fee88fc
Diogo Sobral <dsforasteiro@gmail.com>
2025-05-02 13:46:39 -0300

    Bottom menu icons updated

--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Dimensions, View, TouchableOpacity } from 'react-native';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import { StyleSheet, Dimensions, View, TouchableOpacity, useColorScheme } from 'react-native';
+import Boot from '@/assets/Icons/Boot.svg';
+import Shield from '@/assets/Icons/Shield.svg';
+import Trophy from '@/assets/Icons/Trophy.svg';
 import { useState } from "react";
 import { router } from 'expo-router';
 
@@ -12,15 +12,19 @@ import { Colors } from "@/constants/Colors"
 const windowWidth = Dimensions.get('window').width;
 
 export default function BottomMenu() {
-
+    const theme = useColorScheme() ?? 'light';
     const [selected, setSelected] = useState(1);
 
     const getColor = (index: number) => {
-        return index === selected ? Colors.dark.Red : Colors.dark.DarkerText
+        return index === selected ? Colors[theme].Red : Colors[theme].DarkerText
+    }
+
+    const getBackgroundColor = (index: number) => {
+        return index === selected ? Colors[theme].Red : Colors[theme].DarkBackground
     }
 
     const getIconColor = (index: number) => {
-        return index === selected ? Colors.dark.DarkBackground : Colors.dark.DarkerText
+        return index === selected ? Colors[theme].DarkBackground : Colors[theme].DarkerText
     }
 
     const changePage = (index: number, route: any) => {
@@ -28,30 +32,65 @@ export default function BottomMenu() {
         router.replace(route);
     }
 
+    const styles = StyleSheet.create({
+        menu: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+            width: windowWidth+2,
+            height: 100,
+            left: -1,
+            bottom: -1,
+            borderTopLeftRadius: 12,
+            borderTopRightRadius: 12,
+            borderWidth: 0.5,
+            borderColor: Colors.dark.Red
+        },
+        item: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            fontFamily: "Kokoro",
+            gap: 8,
+            minWidth: 20,
+            maxWidth: 90,
+            width: windowWidth * 0.25
+        },
+        selectedBackground: {
+            // backgroundColor: Colors[theme].Red,
+            borderRadius: 10,
+            paddingVertical: 3,
+        }
+    });
+
     return (
         <ThemedView darkColor={Colors.dark.LightBackground} lightColor={Colors.light.LightBackground}>
             <ThemedView style={styles.menu} darkColor={Colors.dark.DarkBackground} lightColor={Colors.light.DarkBackground}>
 
                 <TouchableOpacity onPress={() => (changePage(0, "/Times"))}>
                     <View style={styles.item} >
-                        <View style={[styles.selectedBackground, selected == 0 ? {opacity: 1} : {opacity: 0}]} />
-                        <MaterialCommunityIcons name="shield-outline" size={30} color={getIconColor(0)} />
+                        <View style={[styles.selectedBackground, {backgroundColor: getBackgroundColor(0)}]}>
+                            <Shield fill={getIconColor(0)} width={60} height={30} />
+                        </View>
                         <ThemedText darkColor={getColor(0)}>Times</ThemedText>
                     </View>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => changePage(1, "/")}>
                     <View style={styles.item} >
-                        <View style={[styles.selectedBackground, selected == 1 ? {opacity: 1} : {opacity: 0}]} />
-                        <SimpleLineIcons name="trophy" size={30} color={getIconColor(1)} />
+                        <View style={[styles.selectedBackground, {backgroundColor: getBackgroundColor(1)}]}>
+                            <Trophy fill={getIconColor(1)} width={60} height={30} />
+                        </View>
                         <ThemedText darkColor={getColor(1)}>Ligas</ThemedText>
                     </View>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => changePage(2, "/Jogadores")}>
                     <View style={styles.item} >
-                        <View style={[styles.selectedBackground, selected == 2 ? {opacity: 1} : {opacity: 0}]} />
-                        <FontAwesome6 name="person-running" size={30} color={getIconColor(2)} />
+                        <View style={[styles.selectedBackground, {backgroundColor: getBackgroundColor(2)}]}>
+                            <Boot fill={getIconColor(2)} width={60} height={30} />
+                        </View>
                         <ThemedText darkColor={getColor(2)}>Jogadores</ThemedText>
                     </View>
                 </TouchableOpacity>
@@ -60,38 +99,3 @@ export default function BottomMenu() {
         </ThemedView>
     );
 }
-
-const styles = StyleSheet.create({
-    menu: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center",
-        width: windowWidth+2,
-        height: 100,
-        left: -1,
-        bottom: -1,
-        borderTopLeftRadius: 12,
-        borderTopRightRadius: 12,
-        borderWidth: 0.5,
-        borderColor: Colors.dark.Red
-    },
-    item: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        fontFamily: "Kokoro",
-        gap: 8,
-        minWidth: 20,
-        maxWidth: 90,
-        width: windowWidth * 0.25
-    },
-    selectedBackground: {
-        position: "absolute",
-        top: -2.5,
-        backgroundColor: Colors.dark.Red,
-        borderRadius: 12,
-        width: 60,
-        height: 35,
-    }
-});
